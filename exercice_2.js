3f8ef7a0d7fa9cd2fb56928f0a63b4071c70b42a
Anas Abou Shaar <seether.anas2911@gmail.com>
2022-04-04 12:09:31 +0200

    exercice_2.js

--- exercice_2.js
+++ exercice_2.js
@@ -12,13 +12,52 @@ const visiteurs = [
 /**
  * 1. Vérifier si le nom de famille "Meineck" existe dans le tableau. Afficher un message en conséquence.
  */
+ 
+// solution 1
+
+ for (let i = 0; i < visiteurs.length; i++) {
+  
+  // console.log (visiteurs[index]);
+  
+   for(let j = 0; j < visiteurs[j].length; j++){
+//console.log(visiteurs[i][j]);
+
+  if(visiteurs[i][j] === 'Meineck' ){
+console.log("Heya there , whatsup?");
+break;
+  }
+  else {
+    console.log("I dont know you");
+  }
+
+   }
+  };
+
+// solution 2
+// includes() permet de verifier si une valeur est presents
+//Dans le tableau
+
+visiteurs.forEach(visiteur => {
+
+  if(visiteur.includes('Meineck')){
+console.log("Heya");
+  }
+});
 
 
 /**
  * 2. Trier le tableau par ordre alphabétique selon les prénoms.
  */
 
+visiteurs.sort();
+console.log(visiteurs);
+
 
 /**
  * 3. Créer un tableau contenant que les adresses e-mails en utilisant l'instruction map().
  */
+
+const emails = visiteurs.map(visiteur =>{
+  return visiteur[2]
+});
+console.log(emails);
